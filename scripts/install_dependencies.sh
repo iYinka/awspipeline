7d5ad76866065e87aeda239f838ecb244d4e031f
iYinka <olayinkav28@gmail.com>
2024-08-31 00:00:57 +0100

    added appspec.yml for AWS CodeDeploy

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo yum update -y
+
+# Install Node.js using NodeSource repository
+curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
+sudo yum install -y nodejs
+
+# Verify installation of Node.js and npm
+node -v
+npm -v
+
+# Install 'forever' globally
+sudo npm install -g forever
+
+#!/bin/bash
+
+mkdir /home/ec2-user/app
+cd /home/ec2-user/app
+
+sudo npm install
