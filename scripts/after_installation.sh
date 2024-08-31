bf6923d6830d24b326263c9dd67323befff71666
iYinka <olayinkav28@gmail.com>
2024-08-31 06:06:44 +0100

    path cp /path/to/source/.babelrc fixed

--- scripts/after_installation.sh
+++ scripts/after_installation.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ ! -f /home/ec2-user/app/.babelrc ]; then
+    cp /app/.babelrc /home/ec2-user/app/
+fi
+
 cd /home/ec2-user/app
 sudo npm i
--- scripts/stop_app_server.sh
+++ scripts/stop_app_server.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ ! -f /home/ec2-user/app/.babelrc ]; then
-    cp /path/to/source/.babelrc /home/ec2-user/app/
-fi
-
 forever stopall
