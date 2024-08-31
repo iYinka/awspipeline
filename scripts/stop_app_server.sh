559125fe6ee2a3eaff22c66d8d172f959c69fa24
iYinka <olayinkav28@gmail.com>
2024-08-31 06:49:59 +0100

    path cp /path/to/source/.babelrc fixed

--- scripts/stop_app_server.sh
+++ scripts/stop_app_server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# Check if .babelrc file already exists and copy only if it doesn't
+if [ ! -f /home/ec2-user/app/.babelrc ]; then
+    cp ~/app/.babelrc /home/ec2-user/app/
+fi
+
 forever stopall
